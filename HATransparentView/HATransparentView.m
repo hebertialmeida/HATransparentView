dd237b4b4ba0c8c19a4dd190cb2d46f642074f70
nickevin <nickevin@gmail.com>
2014-08-14 21:42:26 +0800

    Add tap gesture to close view.

--- HATransparentView/HATransparentView.m
+++ HATransparentView/HATransparentView.m
@@ -12,86 +12,112 @@
 
 @interface HATransparentView ()
 
-@property (nonatomic, assign) NSInteger statusBarStyle;
+@property(nonatomic, assign) NSInteger statusBarStyle;
 
 @end
-
 
 @implementation HATransparentView
 
 #pragma mark - Initialization
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        self.frame = [[UIScreen mainScreen] bounds];
-        self.opaque = NO;
-        self.backgroundColor = kDefaultBackground;
-        self.hideCloseButton = NO;
-    }
-    return self;
+- (id)initWithFrame:(CGRect)frame {
+  self = [super initWithFrame:frame];
+  if (self) {
+    self.frame = [[UIScreen mainScreen] bounds];
+    self.opaque = NO;
+    self.backgroundColor = kDefaultBackground;
+    self.hideCloseButton = NO;
+    self.tapBackgroundToClose = NO;
+  }
+
+  return self;
+}
+
+- (void)tapBackgroundToClose:(BOOL)close {
+  if (close) {
+    [self addTapGestureRecognizer];
+  }
 }
 
 #pragma mark - Open Transparent View
 
-- (void)open
-{
-    // Get main window reference
-    UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    if (!window)
-        window = [[UIApplication sharedApplication].windows objectAtIndex:0];
-    
-    // Get current statusBarStyle
-    self.statusBarStyle = [[UIApplication sharedApplication] statusBarStyle];
-    
-    // Close button
-    if (!self.hideCloseButton) {
-        UIButton *close = [UIButton buttonWithType:UIButtonTypeCustom];
-        close.frame = CGRectMake(self.frame.size.width - 60, 26, 60, 30);
-        [close addTarget:self action:@selector(close) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:close];
-        
-        switch (self.style) {
-            case HAStyleLight: {
-                [close setImage:[UIImage imageNamed:@"btn-close"] forState:UIControlStateNormal];
-                [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-                break;
-            }
-            case HAStyleBlack: {
-                [close setImage:[UIImage imageNamed:@"btn-close-black"] forState:UIControlStateNormal];
-                [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
-                break;
-            }
-        }
-    }
-    
-    // Animation
-    CATransition *viewIn = [CATransition animation];
-    [viewIn setDuration:0.4];
-    [viewIn setType:kCATransitionReveal];
-    [viewIn setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut]];
-    [[self layer] addAnimation:viewIn forKey:kCATransitionReveal];
-    
-    [[[window subviews] objectAtIndex:0] addSubview:self];
-}
+- (void)open {
+  // Get main window reference
+  UIWindow *window = [UIApplication sharedApplication].keyWindow;
+  if (!window)
+    window = [[UIApplication sharedApplication].windows objectAtIndex:0];
 
+  // Get current statusBarStyle
+  self.statusBarStyle = [[UIApplication sharedApplication] statusBarStyle];
+
+  // Close button
+  if (!self.hideCloseButton) {
+    UIButton *close = [UIButton buttonWithType:UIButtonTypeCustom];
+    close.frame = CGRectMake(self.frame.size.width - 60, 26, 60, 30);
+    [close addTarget:self
+                  action:@selector(close)
+        forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:close];
+
+    switch (self.style) {
+    case HAStyleLight: {
+      [close setImage:[UIImage imageNamed:@"btn-close"]
+             forState:UIControlStateNormal];
+      [[UIApplication sharedApplication]
+          setStatusBarStyle:UIStatusBarStyleLightContent];
+      break;
+    }
+    case HAStyleBlack: {
+      [close setImage:[UIImage imageNamed:@"btn-close-black"]
+             forState:UIControlStateNormal];
+      [[UIApplication sharedApplication]
+          setStatusBarStyle:UIStatusBarStyleDefault];
+      break;
+    }
+    }
+  }
+
+  // Animation
+  CATransition *viewIn = [CATransition animation];
+  [viewIn setDuration:0.4];
+  [viewIn setType:kCATransitionReveal];
+  [viewIn
+      setTimingFunction:[CAMediaTimingFunction
+                            functionWithName:kCAMediaTimingFunctionEaseOut]];
+  [[self layer] addAnimation:viewIn forKey:kCATransitionReveal];
+
+  [[[window subviews] objectAtIndex:0] addSubview:self];
+}
 
 #pragma mark - Close Transparent View
 
-- (void)close
-{
-    // Animation
-    CATransition *viewOut = [CATransition animation];
-    [viewOut setDuration:0.3];
-    [viewOut setType:kCATransitionFade];
-    [viewOut setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
-    [[self.superview layer] addAnimation:viewOut forKey:kCATransitionFade];
-    
-    [[UIApplication sharedApplication] setStatusBarStyle:self.statusBarStyle];
-    [self removeFromSuperview];
-    
-    [self.delegate HATransparentViewDidClosed];
+- (void)close {
+  // Animation
+  CATransition *viewOut = [CATransition animation];
+  [viewOut setDuration:0.3];
+  [viewOut setType:kCATransitionFade];
+  [viewOut setTimingFunction:
+               [CAMediaTimingFunction
+                   functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+  [[self.superview layer] addAnimation:viewOut forKey:kCATransitionFade];
+
+  [[UIApplication sharedApplication] setStatusBarStyle:self.statusBarStyle];
+  [self removeFromSuperview];
+
+  [self.delegate didTransparentViewClosed];
+}
+
+#pragma mark - UITapGestureRecognizer
+
+- (void)addTapGestureRecognizer {
+  UITapGestureRecognizer *tapGesture =
+      [[UITapGestureRecognizer alloc] initWithTarget:self
+                                              action:@selector(close:)];
+  [self addGestureRecognizer:tapGesture];
+}
+
+- (void)close:(UITapGestureRecognizer *)sender {
+  [self close];
 }
 
 @end
--- HATransparentViewDemo/ViewController.m
+++ HATransparentViewDemo/ViewController.m
@@ -11,138 +11,211 @@
 
 @interface ViewController () <HATransparentViewDelegate>
 
-@property (strong, nonatomic) HATransparentView *transparentView;
-@property (strong, nonatomic) NSArray *menu;
-@property (strong, nonatomic) NSString *text;
-@property (nonatomic) NSInteger selected;
+@property(strong, nonatomic) HATransparentView *transparentView;
+@property(strong, nonatomic) NSArray *menu;
+@property(strong, nonatomic) NSString *text;
+@property(nonatomic) NSInteger selected;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    _menu = @[@"Your Stations", @"People", @"Alternative", @"Blues", @"Christian/Gospel", @"Classical", @"Country", @"Dance", @"Eletronic", @"Hip Hop", @"Indie", @"Jazz", @"Latin", @"Pop", @"R&B", @"Reggae", @"Rock", @"Songwriters/Folk", @"World", @"More"];
-    
-    _text = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat fermentum diam, et facilisis nisl consectetur eu. Praesent quis blandit felis. Aliquam non vehicula dolor, scelerisque elementum orci. Mauris auctor convallis mauris, sit amet vulputate enim commodo sit amet. Nulla eget diam mollis, lobortis enim eu, egestas ipsum. Nulla et orci nulla. Nunc malesuada turpis vel nisi semper, in sodales ligula venenatis. Fusce quis enim sagittis, ullamcorper quam sed, scelerisque turpis. Pellentesque metus odio, ornare et porttitor a, feugiat nec urna. \n\nSuspendisse luctus, arcu quis placerat volutpat, ligula felis pellentesque lacus, in mattis nunc nibh sed ipsum. Aliquam aliquam elit placerat, porttitor ipsum ut, accumsan massa. Aliquam ut mi vitae elit vulputate consectetur. Ut ac vestibulum massa. Quisque ullamcorper blandit lobortis. In aliquet arcu id odio tempus mollis. Suspendisse quis rhoncus sem, non vulputate urna. Duis porta dui sed auctor porttitor. \n\nEtiam eget orci ultrices, aliquet ligula sed, aliquet felis. Cras nulla purus, volutpat nec leo aliquet, imperdiet lacinia sem. Curabitur lobortis fringilla lacus, elementum pharetra elit dapibus in. Praesent eget eros ut tortor dictum tincidunt. Donec feugiat tellus lectus, id egestas turpis semper vitae. Donec scelerisque magna quis pulvinar congue. Donec eu nisi nulla. Maecenas ornare volutpat orci, ut elementum libero euismod ut. Maecenas condimentum tellus at cursus lobortis. Fusce sapien lorem, hendrerit dapibus sapien eget, tempor laoreet purus. Vivamus quis blandit lacus. \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat fermentum diam, et facilisis nisl consectetur eu. Praesent quis blandit felis. Aliquam non vehicula dolor, scelerisque elementum orci. Mauris auctor convallis mauris, sit amet vulputate enim commodo sit amet. Nulla eget diam mollis, lobortis enim eu, egestas ipsum. Nulla et orci nulla. Nunc malesuada turpis vel nisi semper, in sodales ligula venenatis. Fusce quis enim sagittis, ullamcorper quam sed, scelerisque turpis. Pellentesque metus odio, ornare et porttitor a, feugiat nec urna. \n\nSuspendisse luctus, arcu quis placerat volutpat, ligula felis pellentesque lacus, in mattis nunc nibh sed ipsum. Aliquam aliquam elit placerat, porttitor ipsum ut, accumsan massa. Aliquam ut mi vitae elit vulputate consectetur. Ut ac vestibulum massa. Quisque ullamcorper blandit lobortis. In aliquet arcu id odio tempus mollis. Suspendisse quis rhoncus sem, non vulputate urna. Duis porta dui sed auctor porttitor. \n\nEtiam eget orci ultrices, aliquet ligula sed, aliquet felis. Cras nulla purus, volutpat nec leo aliquet, imperdiet lacinia sem. Curabitur lobortis fringilla lacus, elementum pharetra elit dapibus in. Praesent eget eros ut tortor dictum tincidunt. Donec feugiat tellus lectus, id egestas turpis semper vitae. Donec scelerisque magna quis pulvinar congue. Donec eu nisi nulla. Maecenas ornare volutpat orci, ut elementum libero euismod ut. Maecenas condimentum tellus at cursus lobortis. Fusce sapien lorem, hendrerit dapibus sapien eget, tempor laoreet purus. Vivamus quis blandit lacus.";
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  _menu = @[
+    @"Your Stations",
+    @"People",
+    @"Alternative",
+    @"Blues",
+    @"Christian/Gospel",
+    @"Classical",
+    @"Country",
+    @"Dance",
+    @"Eletronic",
+    @"Hip Hop",
+    @"Indie",
+    @"Jazz",
+    @"Latin",
+    @"Pop",
+    @"R&B",
+    @"Reggae",
+    @"Rock",
+    @"Songwriters/Folk",
+    @"World",
+    @"More"
+  ];
+
+  _text =
+      @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat "
+      @"fermentum diam, et facilisis nisl consectetur eu. Praesent quis "
+      @"blandit felis. Aliquam non vehicula dolor, scelerisque elementum "
+      @"orci. Mauris auctor convallis mauris, sit amet vulputate enim commodo "
+      @"sit amet. Nulla eget diam mollis, lobortis enim eu, egestas ipsum. "
+      @"Nulla et orci nulla. Nunc malesuada turpis vel nisi semper, in "
+      @"sodales ligula venenatis. Fusce quis enim sagittis, ullamcorper quam "
+      @"sed, scelerisque turpis. Pellentesque metus odio, ornare et porttitor "
+      @"a, feugiat nec urna. \n\nSuspendisse luctus, arcu quis placerat "
+      @"volutpat, ligula felis pellentesque lacus, in mattis nunc nibh sed "
+      @"ipsum. Aliquam aliquam elit placerat, porttitor ipsum ut, accumsan "
+      @"massa. Aliquam ut mi vitae elit vulputate consectetur. Ut ac "
+      @"vestibulum massa. Quisque ullamcorper blandit lobortis. In aliquet "
+      @"arcu id odio tempus mollis. Suspendisse quis rhoncus sem, non "
+      @"vulputate urna. Duis porta dui sed auctor porttitor. \n\nEtiam eget "
+      @"orci ultrices, aliquet ligula sed, aliquet felis. Cras nulla purus, "
+      @"volutpat nec leo aliquet, imperdiet lacinia sem. Curabitur lobortis "
+      @"fringilla lacus, elementum pharetra elit dapibus in. Praesent eget "
+      @"eros ut tortor dictum tincidunt. Donec feugiat tellus lectus, id "
+      @"egestas turpis semper vitae. Donec scelerisque magna quis pulvinar "
+      @"congue. Donec eu nisi nulla. Maecenas ornare volutpat orci, ut "
+      @"elementum libero euismod ut. Maecenas condimentum tellus at cursus "
+      @"lobortis. Fusce sapien lorem, hendrerit dapibus sapien eget, tempor "
+      @"laoreet purus. Vivamus quis blandit lacus. \n\nLorem ipsum dolor sit "
+      @"amet, consectetur adipiscing elit. Sed consequat fermentum diam, et "
+      @"facilisis nisl consectetur eu. Praesent quis blandit felis. Aliquam "
+      @"non vehicula dolor, scelerisque elementum orci. Mauris auctor "
+      @"convallis mauris, sit amet vulputate enim commodo sit amet. Nulla "
+      @"eget diam mollis, lobortis enim eu, egestas ipsum. Nulla et orci "
+      @"nulla. Nunc malesuada turpis vel nisi semper, in sodales ligula "
+      @"venenatis. Fusce quis enim sagittis, ullamcorper quam sed, "
+      @"scelerisque turpis. Pellentesque metus odio, ornare et porttitor a, "
+      @"feugiat nec urna. \n\nSuspendisse luctus, arcu quis placerat "
+      @"volutpat, ligula felis pellentesque lacus, in mattis nunc nibh sed "
+      @"ipsum. Aliquam aliquam elit placerat, porttitor ipsum ut, accumsan "
+      @"massa. Aliquam ut mi vitae elit vulputate consectetur. Ut ac "
+      @"vestibulum massa. Quisque ullamcorper blandit lobortis. In aliquet "
+      @"arcu id odio tempus mollis. Suspendisse quis rhoncus sem, non "
+      @"vulputate urna. Duis porta dui sed auctor porttitor. \n\nEtiam eget "
+      @"orci ultrices, aliquet ligula sed, aliquet felis. Cras nulla purus, "
+      @"volutpat nec leo aliquet, imperdiet lacinia sem. Curabitur lobortis "
+      @"fringilla lacus, elementum pharetra elit dapibus in. Praesent eget "
+      @"eros ut tortor dictum tincidunt. Donec feugiat tellus lectus, id "
+      @"egestas turpis semper vitae. Donec scelerisque magna quis pulvinar "
+      @"congue. Donec eu nisi nulla. Maecenas ornare volutpat orci, ut "
+      @"elementum libero euismod ut. Maecenas condimentum tellus at cursus "
+      @"lobortis. Fusce sapien lorem, hendrerit dapibus sapien eget, tempor "
+      @"laoreet purus. Vivamus quis blandit lacus.";
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)didPressedDemo:(UIButton *)sender
-{
-    _transparentView = [[HATransparentView alloc] init];
-    _transparentView.delegate = self;
-    [_transparentView open];
+- (IBAction)didPressedDemo:(UIButton *)sender {
+  _transparentView = [[HATransparentView alloc] init];
+  _transparentView.delegate = self;
+  [_transparentView open];
 
-    // Add a tableView
-    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64, _transparentView.frame.size.width, _transparentView.frame.size.height - 64)];
-    _tableView.delegate = self;
-    _tableView.dataSource = self;
-    _tableView.backgroundColor = [UIColor clearColor];
-    _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    
-    [_transparentView addSubview:_tableView];
+  // Add a tableView
+  _tableView = [[UITableView alloc]
+      initWithFrame:CGRectMake(0, 64, _transparentView.frame.size.width,
+                               _transparentView.frame.size.height - 64)];
+  _tableView.delegate = self;
+  _tableView.dataSource = self;
+  _tableView.backgroundColor = [UIColor clearColor];
+  _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+
+  [_transparentView addSubview:_tableView];
 }
 
-- (IBAction)didPressedDemo2:(id)sender
-{
-    _transparentView = [[HATransparentView alloc] init];
-    _transparentView.delegate = self;
-    _transparentView.backgroundColor = [UIColor colorWithRed:242/255.0 green:46/255.0 blue:50/255.0 alpha:0.9];
-    [_transparentView open];
-    
-    
-    // Add a textView
-    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(20, 64, _transparentView.frame.size.width - 40, _transparentView.frame.size.height - 64)];
-    textView.text = _text;
-    textView.backgroundColor = [UIColor clearColor];
-    textView.textColor = [UIColor whiteColor];
-    textView.editable = NO;
-    textView.font = [UIFont systemFontOfSize:15];
-    [_transparentView addSubview:textView];
+- (IBAction)didPressedDemo2:(id)sender {
+  _transparentView = [[HATransparentView alloc] init];
+  _transparentView.delegate = self;
+  _transparentView.tapBackgroundToClose = YES;
+  _transparentView.hideCloseButton = YES;
+  _transparentView.backgroundColor = [UIColor colorWithRed:242 / 255.0
+                                                     green:46 / 255.0
+                                                      blue:50 / 255.0
+                                                     alpha:0.9];
+  [_transparentView open];
+
+  // Add a textView
+  UITextView *textView = [[UITextView alloc]
+      initWithFrame:CGRectMake(20, 64, _transparentView.frame.size.width - 40,
+                               _transparentView.frame.size.height - 64)];
+  textView.text = _text;
+  textView.backgroundColor = [UIColor clearColor];
+  textView.textColor = [UIColor whiteColor];
+  textView.editable = NO;
+  textView.font = [UIFont systemFontOfSize:15];
+  [_transparentView addSubview:textView];
 }
 
-- (IBAction)didPressedDemo3:(id)sender
-{
-    _transparentView = [[HATransparentView alloc] init];
-    _transparentView.delegate = self;
-    _transparentView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.9];
-    _transparentView.style = HAStyleBlack;
-    [_transparentView open];
-    
-    // Add a textView
-    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(20, 64, _transparentView.frame.size.width - 40, _transparentView.frame.size.height - 64)];
-    textView.text = _text;
-    textView.backgroundColor = [UIColor clearColor];
-    textView.textColor = [UIColor blackColor];
-    textView.editable = NO;
-    textView.font = [UIFont systemFontOfSize:15];
-    [_transparentView addSubview:textView];
-}
+- (IBAction)didPressedDemo3:(id)sender {
+  _transparentView = [[HATransparentView alloc] init];
+  _transparentView.delegate = self;
+  _transparentView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.9];
+  _transparentView.style = HAStyleBlack;
+  [_transparentView open];
 
+  // Add a textView
+  UITextView *textView = [[UITextView alloc]
+      initWithFrame:CGRectMake(20, 64, _transparentView.frame.size.width - 40,
+                               _transparentView.frame.size.height - 64)];
+  textView.text = _text;
+  textView.backgroundColor = [UIColor clearColor];
+  textView.textColor = [UIColor blackColor];
+  textView.editable = NO;
+  textView.font = [UIFont systemFontOfSize:15];
+  [_transparentView addSubview:textView];
+}
 
 #pragma mark - TableView Datasource
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return _menu.count;
+- (NSInteger)tableView:(UITableView *)tableView
+    numberOfRowsInSection:(NSInteger)section {
+  return _menu.count;
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return 44;
+- (CGFloat)tableView:(UITableView *)tableView
+    heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+  return 44;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell;
-    static NSString *cellId = @"cellId";
-    
-    cell = [tableView dequeueReusableCellWithIdentifier:cellId];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
-    }
-    
-    UIImageView *check = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"check"]];
-    
-    cell.textLabel.text = _menu[indexPath.row];
-    cell.textLabel.textColor = [UIColor whiteColor];
-    cell.backgroundColor = [UIColor clearColor];
-    cell.selectedBackgroundView = [[UIView alloc] initWithFrame:CGRectZero];
-    cell.selectedBackgroundView.backgroundColor = [UIColor colorWithRed:0.0 green:143/255.0 blue:213/255.0 alpha:1.0];
-    cell.accessoryView = (_selected == indexPath.row) ? check : nil;
-    
-    return cell;
-}
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell;
+  static NSString *cellId = @"cellId";
 
+  cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+  if (cell == nil) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                  reuseIdentifier:cellId];
+  }
+
+  UIImageView *check =
+      [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"check"]];
+
+  cell.textLabel.text = _menu[indexPath.row];
+  cell.textLabel.textColor = [UIColor whiteColor];
+  cell.backgroundColor = [UIColor clearColor];
+  cell.selectedBackgroundView = [[UIView alloc] initWithFrame:CGRectZero];
+  cell.selectedBackgroundView.backgroundColor =
+      [UIColor colorWithRed:0.0 green:143 / 255.0 blue:213 / 255.0 alpha:1.0];
+  cell.accessoryView = (_selected == indexPath.row) ? check : nil;
+
+  return cell;
+}
 
 #pragma mark - TableView Delegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    _selected = indexPath.row;
-    
-    // Remove
-    [_transparentView close];
+- (void)tableView:(UITableView *)tableView
+    didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  _selected = indexPath.row;
+
+  // Remove
+  [_transparentView close];
 }
 
 #pragma mark - HATransparentViewDelegate
 
-- (void)HATransparentViewDidClosed
-{
-    NSLog(@"Did close");
+- (void)didTransparentViewClosed {
+  NSLog(@"Did close");
 }
 
 @end
